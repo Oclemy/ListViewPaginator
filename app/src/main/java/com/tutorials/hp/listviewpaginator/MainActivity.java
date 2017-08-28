32368af52fe85bb41ca3f86512455d1832664666
Oclemy <oclemmi@gmail.com>
2017-08-28 08:04:25 +0300

    Updated Source code.

--- app/src/main/java/com/tutorials/hp/listviewpaginator/MainActivity.java
+++ app/src/main/java/com/tutorials/hp/listviewpaginator/MainActivity.java
@@ -8,25 +8,40 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
-
+/*
+- Our MainActivity class.
+- Derives from AppCompatActivity.
+- The views we use are ListView and buttons.
+- By default current page is set to 0, which is actually our first page.
+- So when we run the project, the first page data is generated, with next button enabled while previous button disabled.
+- We can then navigate to next page, then to previous or next pages until the last page. When we reach last page, the next button gets disabled.
+- Methods: onCreate(),toggleButtons().
+ */
 public class MainActivity extends AppCompatActivity {
-
+    /*
+    DECLARATIONS
+     */
     ListView lv;
     Button nextBtn, prevBtn;
     Paginator p = new Paginator();
     private int totalPages = Paginator.TOTAL_NUM_ITEMS / Paginator.ITEMS_PER_PAGE;
     private int currentPage = 0;
 
+    /*
+    WHEN ACTIVITY IS CREATED.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //INIATILZE VIEW.
         lv = (ListView) findViewById(R.id.lv);
         nextBtn = (Button) findViewById(R.id.nextBtn);
         prevBtn = (Button) findViewById(R.id.prevBtn);
         prevBtn.setEnabled(false);
 
+        //SHOW TOAST WHEN LISTVIEW ITEM IS CLICKED
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -34,34 +49,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        //SET ADAPTER TO LISTVIEW
         lv.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, p.generatePage(currentPage)));
 
+        //NAVIGATE TO NEXT PAGE.
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 currentPage += 1;
                 // enableDisableButtons();
                 lv.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, p.generatePage(currentPage)));
                 toggleButtons();
-
             }
         });
+
+        //NAVIGATE TO PREVIOUS PAGE
         prevBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 currentPage -= 1;
-
                 lv.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, p.generatePage(currentPage)));
-
                 toggleButtons();
             }
         });
-
-
     }
-
+    /*
+    TOGGLE NEXT AND PREVIOUS BUTTONS DEPENDING ON CURRENT PAGE
+     */
     private void toggleButtons() {
         if (currentPage == totalPages) {
             nextBtn.setEnabled(false);
@@ -73,6 +87,5 @@ public class MainActivity extends AppCompatActivity {
             nextBtn.setEnabled(true);
             prevBtn.setEnabled(true);
         }
-
     }
 }
--- app/src/main/java/com/tutorials/hp/listviewpaginator/Paginator.java
+++ app/src/main/java/com/tutorials/hp/listviewpaginator/Paginator.java
@@ -3,24 +3,30 @@ package com.tutorials.hp.listviewpaginator;
 import java.util.ArrayList;
 
 /**
- * Created by Oclemy on 9/7/2016 for ProgrammingWizards Channel and http://www.camposha.com.
+ * Created by Oclemy for ProgrammingWizards TV Channel and http://www.camposha.info
+ - Our Paginator class.
+ - responsible for paging/paginating our data.
+ - We first specify soem constants like total number of items in the list, items per page etc.
+ - We create a method that takes a current page integer and returns an arraylist of generated page data.
  */
 public class Paginator {
-
+    /*
+    Constants
+     */
     public static final int TOTAL_NUM_ITEMS=52;
     public static final int ITEMS_PER_PAGE=10;
     public static final int ITEMS_REMAINING=TOTAL_NUM_ITEMS % ITEMS_PER_PAGE;
     public static final int LAST_PAGE=TOTAL_NUM_ITEMS/ITEMS_PER_PAGE;
 
-
+    /*
+    GENERATE PAGE DATA, PASS US CURRENT PAGE.
+     */
     public ArrayList<String> generatePage(int currentPage)
     {
         int startItem=currentPage*ITEMS_PER_PAGE+1;
         int numOfData=ITEMS_PER_PAGE;
 
         ArrayList<String> pageData=new ArrayList<>();
-
-
 
         if (currentPage==LAST_PAGE && ITEMS_REMAINING>0)
         {
@@ -35,8 +41,6 @@ public class Paginator {
                 pageData.add("Number "+i);
             }
         }
-
-
         return pageData;
     }
 }
